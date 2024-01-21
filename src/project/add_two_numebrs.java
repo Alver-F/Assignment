762f24f94b653c23fea2c664494fd402718fa840
Alver-F <rbac22108.rcca@gmail.com>
2024-01-21 22:00:55 +0530

    Commit

--- src/project/add_two_numebrs.java
+++ src/project/add_two_numebrs.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class add_two_numebrs {
 	public static void main(String[] args)
 	{
-		int num1,num2,sum;
+		int n1,n2,sum;
 		Scanner scan=new Scanner(System.in);
 		
 		System.out.println("Enter number1 and number2:");
-		num1=scan.nextInt();
-		num2=scan.nextInt();
-		
-		sum=num1+num2;
-		
+		n1=scan.nextInt();
+		n2=scan.nextInt();
+		sum=n1+n2;
 		System.out.println("Sum:"+sum);
 		
 	
